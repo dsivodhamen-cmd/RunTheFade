cddee6667a347a97047c2251a5e6b0a9050b22eb
dsivodhamen-cmd <dsivodhamen@students.mags.school.nz>
2026-06-05 10:36:42 +1200

    Create movement for P1

---
+++ run-the-fade/scripts/player_1.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+var speed = 300
+# create a varaible storing the players speed
+var jump = -400
+# create a varaible storing the players jump
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+# giving the player gravity making them fall if they are in the air
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = jump
+	else:
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = jump
+# allows the player to jump while on the floor as well as in the air
+	
+	var direction = Input.get_axis("ui_left", "ui_right")
+# A varabile storing the direction of where the player travels
+	if direction: 
+		velocity.x = direction * speed
+
+	move_and_slide()
+# # allows the player to move in the direction they are holding
+
+
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
