f50ef79ad08ff778de84a8ec5cf862ac06ce37b9
dsivodhamen-cmd <dsivodhamen@students.mags.school.nz>
2026-06-19 19:54:43 +1200

    MOST animations for player 1

--- run-the-fade/scripts/player_1.gd
+++ run-the-fade/scripts/player_1.gd
@@ -20,6 +20,9 @@ var is_blocking = false
 # create a varaible storing the players blocking, make it where they arent blocking
 var parry_window = 0.2
 # create a varabile storing the window where the player can parry attacks.
+var is_attacking = false
+# creates a variable storing the players atacking, make it where they arent attacking
+
 
 @export var animation: AnimationPlayer
 # exporting an varaible storing all the animations for player 1
@@ -33,6 +36,8 @@ var parry_window = 0.2
 # exporting the progress bar and making it an variable for storing posture
 @export var p1_hitbox: Area2D
 # exporting an area 2d and making the the varaible for the players hitbox
+@onready var sprite_2d: Sprite2D = $Sprite2D
+# bringst the sprite 2d into the code allowing for it to be referenced (will be used to flip sprite)
 
 
 func _ready() -> void:
@@ -48,34 +53,62 @@ func _ready() -> void:
 # makes p1_hitbox stay on all the time.
 
 func _physics_process(delta: float) -> void:
+	var direction = Input.get_axis("p1_left", "p1_right")
+# A varabile storing the direction of where the player travels
+
+	if is_attacking:
+		move_and_slide()
+		return
+# If the player is attacking then this will prevent the player from moving
+	if is_parrying:
+		move_and_slide()
+		return
+# If the player is parrying then this will prevent the player from moving
+
+	if direction > 0:
+		sprite_2d.flip_h = false
+# If the player is moving to the right then the sprite will not flip
+	elif direction < 0:
+		sprite_2d.flip_h = true
+# If the player is moving to the left then the sprite will flip facing the right direction.
+
+
+	if direction: 
+		velocity.x = direction * speed
+		
+# allows the player to move in the direction they are holding
+		if is_on_floor():
+			animation.play("walking")
+# Plays the walking animation if the player is moving while on the floor
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+# if the player lets go they will stop moving
+		if is_on_floor():
+			animation.play("idle")
+# Plays the idle animation if the player isnt moving while on the floor.
+	
 	if is_on_floor():
 		jumps_left = max_jumps
 # if the player is on the floor the player gets the maximum ammount of jumps
 		
 		if Input.is_action_just_pressed("p1_up"):
 			velocity.y = jump
-# allows the player to jump when they are on the floor
+			animation.play("jumping")
+			jumps_left -= 1
+# allows the player to jump when they are on the floor and plays the jumping animation
 
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 # giving the player gravity making them fall if they are in the air
+		if velocity.y > 0:
+			animation.play("falling")
+# if the player is falling (negative y) then the falling animation will play
 		
 		if Input.is_action_just_pressed("p1_up") and jumps_left > 0:
 			velocity.y = jump
+			animation.play("jumping")
 			jumps_left -= 1
-# allows the player to jump in the air if they have enough jumps left
-	
-	var direction = Input.get_axis("p1_left", "p1_right")
-# A varabile storing the direction of where the player travels
-	if direction: 
-		velocity.x = direction * speed
-		
-# allows the player to move in the direction they are holding
-		animation.play("walking")
-# Plays the walking animation
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-# if the player lets go they will stop moving
+# allows the player to jump in the air if they have enough jumps left and plays the jumping animation
 
 	move_and_slide()
 # makes the player move
@@ -90,6 +123,7 @@ func _process(delta: float) -> void:
 	
 	if Input.is_action_pressed("p1_block") and !is_parrying:
 		is_blocking = true
+		animation.play("Block")
 # if the player holds the block button and misses the parry window then they will block.
 	
 	else:
@@ -107,18 +141,31 @@ func _process(delta: float) -> void:
 
 
 func light_attack():
+	if is_attacking:
+		return
+# returns the function if the player is attacking
+	
+	is_attacking = true 
+# sets attacking to true
+
 	if Input.is_action_pressed("p1_down"):
 # if the player is holding down when they attack then it will down light
-		print("Down L attack")
+		animation.play("Light down attack")
+# plays the down light attack animation
 		
 	elif Input.is_action_pressed("p1_left") or Input.is_action_pressed("p1_right"):
 # if the play is holding right or left when they attack it will side light
-		print("Side L attack")
+		animation.play("Light side attack")
+# plays the side light attack animation
 		
 	else:
-		print("Up L attack")
-# if the player is not holding anything when they attack then it will Up attack
-		
+		animation.play("Light up attack")
+# if the player is not holding anything when they attack then it will Up attack	
+
+	await animation.animation_finished 
+	is_attacking = false 
+# waits for the animation to end and then makes the player stop attacking
+	
 	for area in p1_hitbox.get_overlapping_areas():
 # a for loop going through for all areas inside of p1_hitbox
 		if area.get_parent() != self:
@@ -127,17 +174,30 @@ func light_attack():
 
 
 func heavy_attack():
+	if is_attacking:
+		return
+# returns the function if the player is already attacking
+	
+	is_attacking = true
+# sets attacking to true
+	
 	if Input.is_action_pressed("p1_down"):
 # if the player is holding down when they attack then it will down heavy
-		print("Down H attack")
+		animation.play("Heavy down attack")
+# plays the heavy down attack animation
 		
 	elif Input.is_action_pressed("p1_left") or Input.is_action_pressed("p1_right"):
 # if the play is holding right or left when they attack it will side heavy
-		print("Side H attack")
+		animation.play("Heavy side attack")
+# plays the heavy side attack animation
 		
 	else:
-		print("Up H attack")
-# if the player is not holding anything when they attack then it will Up heavy
+		animation.play("Heavy up attack")
+# if the player is not holding anything when they attack then it will Up heavy and play animation
+		
+	await animation.animation_finished 
+	is_attacking = false 
+# waits for the animation to end and then makes the player stop attacking
 		
 	for area in p1_hitbox.get_overlapping_areas():
 # a for loop going through all areas inside of p1_hitbox
@@ -149,6 +209,8 @@ func heavy_attack():
 func start_parry():
 	is_parrying = true
 # when the player presses the block button then it will turn on parry
+	animation.play("Parry")
+# plays the parry animation
 	
 	await get_tree().create_timer(parry_window).timeout
 	is_parrying = false
@@ -164,10 +226,8 @@ func take_damage(amount):
 		p1_posture_ui.value = p1_posture
 # when the player lands a parry they will gain 15 posture.
 		
-		print("parry")
-		
 		return
-# returns the function allowing the player to parry again immediately
+# returns the function
 	
 	
 	if is_blocking:
