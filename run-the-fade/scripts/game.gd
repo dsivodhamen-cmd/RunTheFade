826517873eff987d5023f6403141665134a71d0a
dsivodhamen-cmd <dsivodhamen@students.mags.school.nz>
2026-08-09 01:57:50 +1200

    created multiple maps and started coding randomize scripts

---
+++ run-the-fade/scripts/game.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var maps = [
+	preload("res://scenes/map_1.tscn"),
+	preload("res://scenes/map_2.tscn"),
+	preload("res://scenes/map_3.tscn"),
+	preload("res://scenes/map_4.tscn"),
+	preload("res://scenes/map_5.tscn")
+]
+# creates a varaible that store all maps and preloads them in a list
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+
+	randomize()
+# creates a randomize function
+
+var random_map = maps.pick_random()
+# picks a random map out of the list and stores it in a varaible
+var map_instance = random_map.instantiate()
+# creates that map in the game and stores it in an varaible.
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
